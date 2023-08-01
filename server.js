4a5e90f6ca6d82f7deb7a9d07dae567479be1694
tombryson <thomas.bryson@outlook.com>
2023-08-01 17:15:19 +1000

    changes to server

--- server.js
+++ server.js
@@ -6,69 +6,56 @@ const app = express();
 const port = 3000;
 
 // Your existing endpoints...
-app.post('/receiveData', express.json(), (req, res) => {
-  const data = req.body;  // Data sent by the client
-
-  // Connect to the MongoDB client
+app.post('/receiveData', express.json(), async (req, res) => {
+  const data = req.body;
   const uri = process.env.MONGO_URI;
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  client.connect(err => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error connecting to database.');
-      return;
-    }
+  try {
+    await client.connect();
 
     const collection = client.db("pomodoroDB").collection("pomodoroData");
 
-    // Insert the data into the collection
-    collection.insertOne(data, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error inserting data into database.');
-        return;
-      }
+    await collection.insertOne(data);
 
-      console.log('Data inserted into database.');
-      res.status(200).json({ message: 'Data received and inserted successfully.' });
-    });
-
-    // Close the connection to the client
-    client.close();
-  });
+    console.log('Data inserted into database.');
+    res.status(200).json({ message: 'Data received and inserted successfully.' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error processing request.');
+  } finally {
+    await client.close();
+  }
 });
 
-app.get('/sendToWebhook', (req, res) => {
+
+app.get('/viewData', async (req, res) => { // <-- 'async' keyword is here
   const uri = process.env.MONGO_URI;
+  console.log(uri);
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  client.connect(err => {
-    if (err) {
-      res.status(500).send(err);
-      return;
-    }
+  try {
+    await client.connect(); // <-- 'await' keyword is inside async function
 
     const collection = client.db("pomodoroDB").collection("pomodoroData");
 
-    collection.find({}).toArray((err, documents) => {
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
+    const documents = await collection.find({}).toArray();
 
-      for (let document of documents) {
-        axios.post(process.env.WEBHOOK_URL, document)
-          .then(response => console.log(response.status))
-          .catch(error => console.log(error));
-      }
+    let data = '';
 
-      client.close();
-    });
-  });
+    for (let document of documents) {
+      data += `<p>${JSON.stringify(document)}</p>`;
+    }
 
-  res.status(200).send("Webhook requests sent.");
+    res.status(200).send(`<h1>Data from MongoDB</h1>${data}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving data.');
+  } finally {
+    await client.close();
+  }
 });
+
 
 app.get('/', (req, res) => {
   res.send(`
